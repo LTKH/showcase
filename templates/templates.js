cf4640062cda68cd84c1fc32c46f87ebb2430085
Dmitry Litokh <dmitry@MacBook-Pro-Dmitry.local>
2020-12-11 19:50:41 +1000

    create new repo

---
+++ templates/templates.js
@@ -0,0 +1,9 @@
+var templates = [
+    { 'url': '^/kubernetes.*', 'tmpl': '/templates/alerts.html' },
+    { 'url': '^/vmetrics/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
+    { 'url': '^/vmetrics/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
+    { 'url': '^/vmetrics.*', 'tmpl': '/templates/content.html' },
+    { 'url': '^/prom.*', 'tmpl': '/templates/content.html' },
+    { 'url': '^/monitoring.*', 'tmpl': '/templates/alerts.html' },
+    //{ 'url': '^/.*', 'tmpl': '/templates/main.html' }
+];
