02efae0b9abf5a74c3461de68cce18d47c57a697
Dmitry Litokh <dmitry@MacBook-Pro-Dmitry.local>
2020-12-13 10:45:51 +1000

    test

--- templates/templates.js
+++ templates/templates.js
@@ -2,7 +2,7 @@ var templates = [
     { 'url': '^/kubernetes.*', 'tmpl': '/templates/alerts.html' },
     { 'url': '^/vmetrics/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
     { 'url': '^/vmetrics/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
-    { 'url': '^/vmetrics.*', 'tmpl': '/templates/content.html' },
+    { 'url': '^/vmetrics.*', 'tmpl': '/templates/vmetrics/main.html' },
     { 'url': '^/prom.*', 'tmpl': '/templates/content.html' },
     { 'url': '^/monitoring.*', 'tmpl': '/templates/alerts.html' },
     //{ 'url': '^/.*', 'tmpl': '/templates/main.html' }
