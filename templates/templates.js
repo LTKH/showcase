e703af4cf15f4c5cf6bf8db026bc521780e153f4
Dmitry Litokh <da.litokh@gmail.com>
2020-12-26 09:01:04 +0300

    app.min.css

--- templates/templates.js
+++ templates/templates.js
@@ -2,6 +2,8 @@ var templates = [
     { 'url': '^/kubernetes.*', 'tmpl': '/templates/alerts.html' },
     { 'url': '^/vmetrics/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
     { 'url': '^/vmetrics/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
+    { 'url': '^/prometheus/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
+    { 'url': '^/prometheus/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
     { 'url': '^/vmetrics.*', 'tmpl': '/templates/vmetrics/main.html' },
     { 'url': '^/prom.*', 'tmpl': '/templates/content.html' },
     { 'url': '^/monitoring.*', 'tmpl': '/templates/alerts.html' },
