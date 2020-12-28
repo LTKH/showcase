54abedba2a0ee375374546029d34f5ae8b6ff23a
Dmitry Litokh <da.litokh@gmail.com>
2020-12-28 22:19:20 +0300

    graph.html

--- templates/templates.js
+++ templates/templates.js
@@ -4,7 +4,7 @@ var templates = [
     { 'url': '^/vmetrics/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
     { 'url': '^/prometheus/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
     { 'url': '^/prometheus/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
-    { 'url': '^/vmetrics.*', 'tmpl': '/templates/vmetrics/main.html' },
+    { 'url': '^/vmetrics.*', 'tmpl': '/templates/vmetrics/graph.html' },
     { 'url': '^/prom.*', 'tmpl': '/templates/content.html' },
     { 'url': '^/monitoring.*', 'tmpl': '/templates/alerts.html' },
     //{ 'url': '^/.*', 'tmpl': '/templates/main.html' }
