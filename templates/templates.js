31d90e32f465c5e483c3ee757ab47e2e4059bb3c
da.litokh <da.litokh@gmail.com>
2021-04-23 13:26:11 +0300

    templates

--- templates/templates.js
+++ templates/templates.js
@@ -1,12 +1,9 @@
 var templates = [
-    { 'url': '^/netmap', 'tmpl': '/templates/netmap/main.html' },
-    { 'url': '^/kubernetes.*', 'tmpl': '/templates/alerts.html' },
-    { 'url': '^/vmetrics/targets', 'tmpl': '/templates/vmetrics/targets.html' },
-    { 'url': '^/vmetrics/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
+    { 'url': '^/netmap.*', 'tmpl': '/templates/netmap/records.html' },
+    { 'url': '^/alerts.*', 'tmpl': '/templates/alerttrap/alerts.html' },
+    { 'url': '^/kubernetes.*', 'tmpl': '/templates/alerttrap/alerts.html' },
+    { 'url': '^/vmetrics/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
+    { 'url': '^/vmetrics/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
     { 'url': '^/prometheus/[^/]+/targets', 'tmpl': '/templates/vmetrics/targets.html' },
     { 'url': '^/prometheus/[^/]+/alerts', 'tmpl': '/templates/vmetrics/alerts.html' },
-    { 'url': '^/vmetrics.*', 'tmpl': '/templates/vmetrics/graph.html' },
-    { 'url': '^/prom.*', 'tmpl': '/templates/content.html' },
-    { 'url': '^/monitoring.*', 'tmpl': '/templates/alerts.html' },
-    //{ 'url': '^/.*', 'tmpl': '/templates/main.html' }
 ];
